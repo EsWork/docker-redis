2852e3897147ce97044f140629602675c42dc401
v.la <v.la@live.cn>
2016-08-10 10:29:56 +0800

    redis use real user

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ REDIS_PASSWORD=${REDIS_PASSWORD:-}
 
 mkdir -p ${REDIS_DATA_DIR}
 chmod -R 0755 ${REDIS_DATA_DIR}
-chown -R ${REDIS_USER}:root ${REDIS_DATA_DIR}
+chown -R ${REDIS_USER}:${REDIS_USER} ${REDIS_DATA_DIR}
 
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
@@ -17,7 +17,7 @@ fi
 echo ${1}
 if [[ -z ${1} ]]; then
   echo "Starting redis-server..."
-  exec $(which redis-server) /etc/redis/redis.conf \
+  exec su-exec $(which redis-server) /etc/redis/redis.conf \
   ${REDIS_PASSWORD:+--requirepass $REDIS_PASSWORD} ${EXTRA_ARGS}
 else
   exec "$@"
--- setup/install.sh
+++ setup/install.sh
@@ -3,12 +3,12 @@ set -e
 
 REDIS_DOWNLOAD_URL=http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
 
+RUNTIME_DEPENDENCIES="su-exec>=0.2"
 BUILD_DEPENDENCIES="gcc linux-headers make musl-dev"
 
-apk add --no-cache ${BUILD_DEPENDENCIES}
+apk add --no-cache ${BUILD_DEPENDENCIES} ${RUNTIME_DEPENDENCIES}
 
 addgroup -S redis && adduser -S -G ${REDIS_USER} ${REDIS_USER}
-#apk add --no-cache 'su-exec>=0.2'
 
 mkdir -p ${REDIS_SETUP_DIR}/src/
 cd ${REDIS_SETUP_DIR}/src/
@@ -29,11 +29,11 @@ sed '/^logfile/d' -i /etc/redis/redis.conf
 
 mkdir -p /run/redis
 chmod -R 0755 /run/redis
-chown -R ${REDIS_USER}:root /run/redis
+chown -R ${REDIS_USER}:${REDIS_USER} /run/redis
 
 mkdir -p ${REDIS_LOG_DIR}
 chmod -R 0755 ${REDIS_LOG_DIR}
-chown -R ${REDIS_USER}:root ${REDIS_LOG_DIR}
+chown -R ${REDIS_USER}:${REDIS_USER} ${REDIS_LOG_DIR}
 
 apk del ${BUILD_DEPENDENCIES}
 rm -rf ${REDIS_SETUP_DIR}/
