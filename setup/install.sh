82a99ff7113193899c0f850e9015a29ae020c297
v.la <v.la@live.cn>
2016-08-10 11:02:56 +0800

    chown etc/redis to redis

--- setup/install.sh
+++ setup/install.sh
@@ -19,7 +19,6 @@ cd ${REDIS_SETUP_DIR}/src/redis-${REDIS_VERSION}
 make && make install
 
 mkdir -p /etc/redis/
-chown -R ${REDIS_USER}:${REDIS_USER} /etc/redis/
 cp redis.conf /etc/redis/redis.conf
 
 sed 's/^daemonize yes/daemonize no/' -i /etc/redis/redis.conf
@@ -27,6 +26,8 @@ sed 's/^bind 127.0.0.1/bind 0.0.0.0/' -i /etc/redis/redis.conf
 sed 's/^# unixsocket \/tmp\/redis.sock/unixsocket \/run\/redis\/redis.sock/' -i /etc/redis/redis.conf
 sed 's/^# unixsocketperm 700/unixsocketperm 777/' -i /etc/redis/redis.conf
 sed '/^logfile/d' -i /etc/redis/redis.conf
+
+chown -R ${REDIS_USER}:${REDIS_USER} /etc/redis/
 
 mkdir -p /run/redis
 chmod -R 0755 /run/redis
